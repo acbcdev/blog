f1a3ca2c25716b4c62899a7fe137c2c643eb4ff6
acbc <acbc.dev@gmail.com>
2025-02-18 20:13:04 -0500

    refactor: clean up configuration and improve layout structure in Home section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,43 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import {
-	transformerNotationDiff,
-	// ...
+  transformerNotationDiff,
+  // ...
 } from "@shikijs/transformers";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	output: "static",
-	integrations: [
-		mdx({
-			optimize: {
-				ignoreElementNames: ["pre"],
-			},
-		}),
-		sitemap(),
-	],
-	markdown: {
-		syntaxHighlight: "shiki",
-		extendDefaultShikiSetup: true,
+  site: "https://example.com",
+  output: "static",
+  integrations: [
+    mdx({
+      optimize: {
+        ignoreElementNames: ["pre"],
+      },
+      syntaxHighlight: "shiki",
+    }),
+    sitemap(),
+  ],
+  markdown: {
+    syntaxHighlight: "shiki",
+    extendDefaultShikiSetup: true,
 
-		shikiConfig: {
-			theme: "tokyo-night",
-			wrap: true,
-			transformers: [transformerNotationDiff],
-		},
-	},
-	i18n: {
-		defaultLocale: "es",
-		locales: ["es", "en"],
-	},
+    shikiConfig: {
+      theme: "tokyo-night",
+      wrap: true,
+      transformers: [transformerNotationDiff],
+    },
+  },
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+      redirectToDefaultLocale: true,
+    },
+  },
+  devToolbar: {
+    enabled: false,
+  },
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,33 +1,34 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-
-
-export const SITE_TITLE = 'The Code';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "The Code";
+export const SITE_DESCRIPTION = "Welcome to my website!";
 
 export const LINKS = {
-  GITHUB: 'https://github.com/ACBC-DEV',
-  TWITTER: 'https://x.com/camilobla1',
-  PORFOLIO: 'https://acbc.vercel.app',
-  LINKEDIN: 'https://www.linkedin.com/in/acbc-dev/'
-} as const
-export const FOOTER = [{
-  name: 'RSS',
-  link: '/rss.xml'
-}, {
-  name: 'Github',
-  link: LINKS.GITHUB
-}, {
-  name: 'Twitter',
-  link: LINKS.TWITTER
-},
-{
-  name: 'Portfolio',
-  link: LINKS.PORFOLIO
-},
-{
-  name: 'Linkedin',
-  link: LINKS.LINKEDIN
-}
-]
+  GITHUB: "https://github.com/acbcdev",
+  TWITTER: "https://x.com/acbcdotdev",
+  PORFOLIO: "https://acbc.dev",
+  LINKEDIN: "https://www.linkedin.com/in/acbc-dev/",
+} as const;
+export const FOOTER = [
+  {
+    name: "RSS",
+    link: "/rss.xml",
+  },
+  {
+    name: "Github",
+    link: LINKS.GITHUB,
+  },
+  {
+    name: "Twitter",
+    link: LINKS.TWITTER,
+  },
+  {
+    name: "Portfolio",
+    link: LINKS.PORFOLIO,
+  },
+  {
+    name: "Linkedin",
+    link: LINKS.LINKEDIN,
+  },
+];
