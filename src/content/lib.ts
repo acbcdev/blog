9e3ba253cbbe4c2fc9f4f4acbbee50dfdcd7f965
acbc <acbc.dev@gmail.com>
2025-02-27 12:32:58 -0500

    feat: add TopicsByLang function for topic retrieval by language and update splitLang function with JSDoc comments

---
+++ src/content/lib.ts
@@ -0,0 +1,15 @@
+import { splitLang } from "@/lib/content";
+import { getCollection } from "astro:content";
+
+export async function TopicsByLang(lang: string) {
+	const posts = await getCollection("blog");
+	const topics = [
+		...new Set(
+			posts
+				.filter((i) => splitLang(i.slug)[0] === lang)
+				.map((post) => post.data.topic)
+				.filter(Boolean),
+		),
+	];
+	return topics;
+}
--- src/lib/content.ts
+++ src/lib/content.ts
@@ -4,7 +4,13 @@
 //   return await getCollection("blog");
 // }
 
-export function splitLang(url: string) {
-  const [lang, rest] = url.split("/");
-  return [lang, rest];
+/**
+ * Splits a URL into its language identifier and the remaining part.
+ *
+ * @param {string} url - The URL string to split.
+ * @returns {[string, string]} An array where the first element is the language and the second is the rest of the URL.
+ */
+export function splitLang(url: string): [string, string] {
+	const [lang, rest] = url.split("/");
+	return [lang, rest];
 }
