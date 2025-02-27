92389c9baf6b59b8216bc120ec89eae54a33e990
acbc <acbc.dev@gmail.com>
2025-02-27 12:32:12 -0500

    feat: update site title and add language constants to consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,34 +1,36 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "The Code";
+export const SITE_TITLE = "ACBC";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 
 export const LINKS = {
-  GITHUB: "https://github.com/acbcdev",
-  TWITTER: "https://x.com/acbcdotdev",
-  PORFOLIO: "https://acbc.dev",
-  LINKEDIN: "https://www.linkedin.com/in/acbc-dev/",
+	GITHUB: "https://github.com/acbcdev",
+	TWITTER: "https://x.com/acbcdotdev",
+	PORFOLIO: "https://acbc.dev",
+	LINKEDIN: "https://www.linkedin.com/in/acbc-dev/",
 } as const;
 export const FOOTER = [
-  {
-    name: "RSS",
-    link: "/rss.xml",
-  },
-  {
-    name: "Github",
-    link: LINKS.GITHUB,
-  },
-  {
-    name: "Twitter",
-    link: LINKS.TWITTER,
-  },
-  {
-    name: "Portfolio",
-    link: LINKS.PORFOLIO,
-  },
-  {
-    name: "Linkedin",
-    link: LINKS.LINKEDIN,
-  },
+	{
+		name: "RSS",
+		link: "/rss.xml",
+	},
+	{
+		name: "Github",
+		link: LINKS.GITHUB,
+	},
+	{
+		name: "Twitter",
+		link: LINKS.TWITTER,
+	},
+	{
+		name: "Portfolio",
+		link: LINKS.PORFOLIO,
+	},
+	{
+		name: "Linkedin",
+		link: LINKS.LINKEDIN,
+	},
 ];
+
+export const LANGS = ["es", "en"] as const;
