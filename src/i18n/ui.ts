488ae586e492a9aeda19b55ffa63951b68e99338
acbc <acbc.dev@gmail.com>
2025-03-06 16:14:12 -0500

    i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,27 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.articles": "Articles",
+    "nav.categories": "Categories",
+    "blog.readMore": "Read more",
+    "blog.publishedOn": "Published on",
+    "blog.categories": "Categories",
+  },
+  es: {
+    "nav.home": "Inicio",
+    "nav.articles": "Artículos",
+    "nav.categories": "Categorías",
+    "blog.readMore": "Leer más",
+    "blog.publishedOn": "Publicado el",
+    "blog.categories": "Categorías",
+  },
+} as const;
+
+export const showDefaultLang = false;
